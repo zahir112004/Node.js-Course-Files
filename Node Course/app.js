48ce6a5fab4e533d56d8017f6aee53ee0f68353d
zahir112004 <pndroidd@gmail.com>
2021-11-17 00:25:16 +0600

    2nd change

---
+++ Node Course/app.js
@@ -0,0 +1,8 @@
+// const validator = require('validator')
+const chalk = require('chalk')
+const getNotes = require('./notes')
+
+console.log(getNotes)
+
+// console.log(validator.isURL('https/www.googm'))
+console.log(chalk.green.bgBlue('Success!'))
